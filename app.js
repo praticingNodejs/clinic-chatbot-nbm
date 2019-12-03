570651e8851f47f014059d729418b5f60a2bd9d6
praticingNodejs <nhuuquan1999@gmail.com>
2019-12-03 08:57:57 +0700

    3 thang 12

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-app.use('/auth',authRouter);
+app.use('/auth', authRouter);
 require('./api/facebook')(app);
 require('./api/zalo')(app);
 require('./api/users')(app);
@@ -34,18 +34,18 @@ require('./api/data')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
